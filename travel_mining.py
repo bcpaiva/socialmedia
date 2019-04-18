218b4a3cac45dc1c2f67370fc2b8eb94918face2
Beau Paiva <beau.paiva@gmail.com>
2019-04-18 14:13:56 -0400

    first commit

---
+++ travel_mining.py
@@ -0,0 +1,16 @@
+#Print out list of the handles of the top ten travel twitter accounts
+print("Is it working")
+
+#Collect previous 1000 tweets from each of the accounts
+
+#Add those tweets to a list, storing each word in the tweets as an indivdual entry in the list
+
+#Use a counter from collections import to create a dictionary giving the frequency of each word
+
+#Extract the locations from the dictionary now !!!!(No idea how were gonna do this)!!!!!
+
+#Retrieve online library of sample list of locations???
+
+#Print out top 10 travel locations
+
+#Create a chart with all the locations that'll look good
